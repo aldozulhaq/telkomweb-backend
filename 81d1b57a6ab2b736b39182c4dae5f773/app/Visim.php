f7604778afac6f031a6cbea3a09f69481e8bd0e2
irgifahrias <fahrias600@gmail.com>
2019-04-20 15:49:33 +0700

    asddff

--- 81d1b57a6ab2b736b39182c4dae5f773/app/Visim.php
+++ 81d1b57a6ab2b736b39182c4dae5f773/app/Visim.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visim extends Model
 {
-    //
+    protected $table = 'sejarah';
+
+    protected $fillable = ['id_vis_post','post_visim','date_visim','flag_visim'];
+
 }
